8fee9625f5850e0a0c1428c8831873e057225190
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2020-12-01 01:22:34 +0000

    Disabled character controller hit for now to fix adding force to doors

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -249,10 +249,10 @@ public class PlayerMovement : MonoBehaviour
     //TEMPORARY TEST FOR COLLIDING WITH OTHER RIGIDBODIES - FIX UP LATER.
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.rigidbody != null)
-        {
-            print("added force");
-            hit.rigidbody.AddForce(hit.moveDirection * 5f);
-        }
+        //if (hit.rigidbody != null)
+        //{
+        //    print("added force");
+        //    hit.rigidbody.AddForce(hit.moveDirection * 5f);
+        //}
     }
 }
