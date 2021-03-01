24814b53a1ce696075992026c72e2b345d6abbab
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2021-03-01 22:07:11 +0000

    Interactable Kettle set up

---
+++ Assets/Scripts/Interactable Stuff/Kettle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kettle : PlayerInteractableObject, iInteractable
+{
+    [Header("Particle System To Turn On")]
+    [SerializeField] private ParticleSystem ParticleSystem;
+
+    public bool IsInteractable { get { return ParticleSystem.isStopped == true; } set {} }
+
+    //Start.
+    public override void Awake()
+    {
+        base.Awake();
+    }
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    //IInteractable.
+    public void PlayerInteracted()
+    {
+        if(IsInteractable)
+        {
+            ParticleSystem.Play();
+            UIManager.Instance.aimDot.Reset();
+        }
+    }
+    public void PlayerLookedAtMe()
+    {
+        if(IsInteractable)
+            UIManager.Instance.aimDot.ChangeToGreen();
+
+    }
+    public void PlayerLookedAwayFromMe()
+    {
+        if(IsInteractable)
+            UIManager.Instance.aimDot.Reset();
+
+    }
+    public void PlayerStoppedInteraction()
+    {
+
+    }
+}
